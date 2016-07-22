41309160b06daedad528ebe701c39396cbf93da1
dudulive <dudurodriguesbraz@gmail.com>
2016-07-21 21:22:53 -0300

    Customização da home page.

--- app/app.ts
+++ app/app.ts
@@ -11,6 +11,7 @@ export class MyApp {
   rootPage: any = HomePage;
 
   constructor(platform: Platform) {
+    this.rootPage = HomePage;
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
